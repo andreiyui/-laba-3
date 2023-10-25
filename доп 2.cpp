1d691af7c01f1402bbb79c0d0382b4633e69135d
andreiyui <147510433+andreiyui@users.noreply.github.com>
2023-10-25 15:12:23 +0300

    Add files via upload

---
+++ доп 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int arr[] = { 5, -2, 10, -8, 3, -6 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Положительные числа: ";
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > 0) {
+            cout << arr[i] << " ";
+        }
+    }
+
+    cout << "\nОтрицательные числа: ";
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            cout << arr[i] << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
---
+++ о # 1 dop.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(0, "");
+    int height = 5; 
+
+    
+    for (int i = 0; i < height; i++) {
+        
+        for (int j = 0; j < height - i - 1; j++) {
+            cout << " ";
+        }
+
+        
+        for (int k = 0; k < 2 * i + 1; k++) {
+            cout << "*";
+        }
+
+        cout << std::endl;
+    }
+
+    
+    for (int i = 0; i < height; i++) {
+        
+        cout << "*";
+        for (int j = 0; j < height - 2; j++) {
+            cout << " ";
+        }
+        cout << "*" << endl;
+    }
+
+   
+    for (int i = 0; i < height; i++) {
+        cout << "*";
+    }
+
+    cout << endl;
+
+    return 0;
+}
